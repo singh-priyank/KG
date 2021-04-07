b74a2185d794a370e094b6aaddc8994eba1dc3d9
singh-priyank <priyankkumar179@gmail.com>
2021-04-07 13:54:06 +0530

    bridge node added

--- kgn.py
+++ kgn.py
@@ -4,17 +4,6 @@ from gremlin_python.process.graph_traversal import __
 from gremlin_python.process.strategies import *
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.process.traversal import T
-from gremlin_python.process.traversal import Order
-from gremlin_python.process.traversal import Cardinality
-from gremlin_python.process.traversal import Column
-from gremlin_python.process.traversal import Direction
-from gremlin_python.process.traversal import Operator
-from gremlin_python.process.traversal import P
-from gremlin_python.process.traversal import Pop
-from gremlin_python.process.traversal import Scope
-from gremlin_python.process.traversal import Barrier
-from gremlin_python.process.traversal import Bindings
-from gremlin_python.process.traversal import WithOptions
 from gremlin_python.structure.graph import Graph
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,8 +12,7 @@ from nltk.corpus import wordnet
 
 
 class Vertex:
-	id = T.id
-	single = Cardinality.single
+
 	g = None
 	def __init__(self,graph):
 		self.g = graph
@@ -56,8 +44,9 @@ class Edge:
 		self.g = graph
 
 	def add_edge(self,label1,label2,prop):
+		g.V().has('vertex','labelV',label1).addE(prop).to(g.V().has('vertex','labelV',label2))
 		#g.V().has('vertex','labelV',label1).as('a').V().has('vertex','labelV',label2).addE(prop).to('a')
-		pass
+
 
 class Import:
 
@@ -67,9 +56,6 @@ class Import:
 	def import_graphml(self, file_name):
 		G1 = nx.read_graphml(file_name)
 		G1 = G1.to_undirected()
-
-		nx.draw(G1,with_labels=True)
-		#plt.show()
 		self.graph = G1
 		return G1
 
@@ -77,16 +63,13 @@ class Import:
 		G2 = nx.Graph()
 		G2.add_nodes_from(nodes)
 		G2.add_edges_from(edges)
-		nx.draw(G2,with_labels=True)
-		#plt.show()
-		#print(G2.nodes(data=True))
-		#self.graph = G2
 		return G2
 
 	def generate_subg(self, node_name, depth=3):
 
 		ego_graph = nx.ego_graph(self.graph, node_name, radius=depth)
-		# plot to check
+		
+		# plot graph 
 		nx.draw(ego_graph,with_labels=True)
 		plt.show()
 
@@ -99,29 +82,51 @@ class Algo:
 
 	def join_graphs(self, graph1, graph2):
 
+		'''  
+			Converting two graph object to single object
+			Adding vertex and edges can be done easily
+		''' 
 		G = nx.compose(graph1,graph2)
 
+		# List of all nodes in a graph
 		g1 = [node for node in graph1.nodes(data=True)]
 		g2 = [node for node in graph2.nodes(data=True)]
-		
+        
+		'''
+			Iterate over each node and find similarity between the nodes
+			If similarity is more than 50% add bridge node to connect both the nodes
+		'''
 		for s in g1:
 			cb = wordnet.synsets(s[1]['labelV'])
 			if len(cb)==0:
 				continue
 			cb = cb[0]
+
 			for e in g2:
 				ib = wordnet.synsets(e[1]['labelV'])
 				if len(ib)==0:
 					continue
 				ib = ib[0]
-				#print(s[1]['labelV'],e[1]['labelV'],ib.wup_similarity(cb))
-				# Condition if similarity is more than 50% and path is already not there 
+
+                # Condition if similarity is more than 50% 
 				if ib.wup_similarity(cb)>=0.5:
-					G.add_edge(s[0],e[0])
-					tt='........'
-					print(s[1]['labelV'],e[1]['labelV'],tt,s[0],e[0])
-					print(ib.wup_similarity(cb))
-		#self.graph = G
+                    
+					# Lowest hypernym will be bridge node
+					bridgess = cb.lowest_common_hypernyms(ib)
+					lemma = bridgess[0].lemmas()
+
+					# If that node is already there add only new edge, else add vertex and edges
+
+					if G.has_node(lemma[0].name()):
+						G.add_edge(e[0],lemma[0].name())
+					else:
+						G.add_nodes_from([(lemma[0].name(),{"labelB":lemma[0].name()}),])
+						G.add_edge(s[0],lemma[0].name())
+						G.add_edge(e[0],lemma[0].name())
+
+					# print(s[1]['labelV'],e[1]['labelV'],tt,s[0],e[0])
+					# print(ib.wup_similarity(cb))
+
 		return G
 
 
@@ -129,9 +134,11 @@ class Algo:
 if __name__=="__main__":
 	gra = Import()
 	G1 = gra.import_graphml('my-graph.graphml')
+
 	nodes = [('1', {"labelV": "college"}), ('2', {"labelV": "department"}), ('3', {"labelV": "professor"}), ('4', {"labelV": "student"}), ('5', {"labelV": "section"}), ]
 	edges = [('1', '2'), ('1', '3'), ('1', '4'), ('4', '5')]
 	G2 = gra.create_graph(nodes,edges)
+	
 	# algo = Algo()
 	# G = algo.join_graphs(G1,G2)
 	#nx.write_graphml(G, "g.graphml")
@@ -144,13 +151,13 @@ if __name__=="__main__":
 
 	''' Add vertex '''
 	data = {'ISBN': '0-394-40135-2', 'year': 1968, 'name': 'The French Chef Cookbook'}
-	vet.add_vertex('book',data)	
+	vet.add_vertex('file',data)	
 
 	#print(vet.list_all())
-	#ed.add_edge('book','file')
+	ed.add_edge('book','file','has')
 
 	
-	print(vet.properties('160'))
+	#print(vet.properties('160'))
 	
 
 	subGraph = g.V('0').repeat(__.bothE().subgraph('subGraph').V()).times(1).cap('subGraph').next()
