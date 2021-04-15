22dc5f312eb69c6015e09a758770c7311eaf6e88
singh-priyank <priyankkumar179@gmail.com>
2021-04-15 23:28:46 +0530

    graphml changed

--- kgn.py
+++ kgn.py
@@ -91,7 +91,7 @@ class Algo:
 			Adding vertex and edges can be done easily
 		''' 
 		G = nx.compose(graph1,graph2)
-
+		l = len(G.nodes)+1
 		# List of all nodes in a graph
 		g1 = [node for node in graph1.nodes(data=True)]
 		g2 = [node for node in graph2.nodes(data=True)]
@@ -101,13 +101,13 @@ class Algo:
 			If similarity is more than 50% add bridge node to connect both the nodes
 		'''
 		for s in g1:
-			cb = wordnet.synsets(s[0])
+			cb = wordnet.synsets(s[1]['labelV'])
 			if len(cb)==0:
 				continue
 			cb = cb[0]
 
 			for e in g2:
-				ib = wordnet.synsets(e[0])
+				ib = wordnet.synsets(e[1]['labelV'])
 				if len(ib)==0:
 					continue
 				ib = ib[0]
@@ -124,9 +124,10 @@ class Algo:
 					if G.has_node(lemma[0].name()):
 						G.add_edge(e[0],lemma[0].name())
 					else:
-						G.add_nodes_from([(lemma[0].name(),{"labelB":"bridge"}),])
-						G.add_edge(s[0],lemma[0].name())
-						G.add_edge(e[0],lemma[0].name())
+						G.add_nodes_from([(l,{"labelB":lemma[0].name()}),])
+						G.add_edge(s[0],l)
+						G.add_edge(e[0],l)
+						l+=1
 
 					# print(s[1]['labelV'],e[1]['labelV'],tt,s[0],e[0])
 					# print(ib.wup_similarity(cb))
@@ -136,7 +137,7 @@ class Algo:
 
 
 if __name__=="__main__":
-	gra = Import()
+	# gra = Import()
 	# G3 = gra.import_graphml('g3.graphml')
 	# G2 = gra.import_graphml('g2.graphml')
 	
@@ -144,40 +145,45 @@ if __name__=="__main__":
 	# G = algo.join_graphs(G3,G2)
 	# nx.write_graphml(G, "gf.graphml")
 
-	G = gra.import_graphml('gf.graphml')
-	#g = traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin','g'))
+	#G = gra.import_graphml('gf.graphml')
+	g = traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin','g'))
 	#g.addV('book').property('name', 'The French Chef Cookbook').property('year' , 1968).property('ISBN', '0-394-40135-2')
-	vet = Vertex(G)
+	#vet = Vertex(G)
 	# ed = Edge(g)
-	print(vet.list_all())
+	#print(vet.list_all())
 
 	
 	''' Add vertex '''
-	# nodes = [('grade',{'labelV':'node', 'grade_id':'INT', 'name':'VARCHAR'}), ('course',{'labelV':'node', 'course_id':'INT', 'name':'VARCHAR', 'grade_id':'INT'}),
-	# 		('classroom',{'labelV':'node', 'classroom_id':'INT', 'grade_id':'INT', 'section':'VARCHAR', 'teacher_id':'INT'}), ('classroom_student',{'labelV':'node','classroom_id':'INT', 'studen_id':'INT'}),
-	# 		('attendance',{'labelV':'node', 'date':'Date', 'student_id':'INT', 'status':'INT'}), ('exam_type',{'exam_type':'INT', 'name':'VARCHAR'}), ('exam',{'exam_id':'INT', 'exam_type':'INT', 'name':'VARCHAR'}),
-	# 		('exam_result',{'labelV':'node', 'exam_id':'INT', 'student_id':'INT', 'course_id':'INT'}), ('student',{'labelV':'node', 'student_id':'INT', 'email':'VARCHAR', 'name':'VARCHAR', 'dob':'Date', 'parent_id':'INT'}),
-	# 		('parent',{'labelV':'node', 'parent_id':'INT', 'email':'VARCHAR', 'password':'VARCHAR', 'mobile':'VARCHAR'}), ('teacher',{'teacher_id':'INT', 'name':'VARCHAR', 'email':'VARCHAR', 'dob':'Date'})]
+	# nodes = [(1, {'labelV': 'grade', 'grade_id': 'INT', 'name': 'VARCHAR'}), (2, {'labelV': 'course', 'course_id': 'INT', 'name': 'VARCHAR', 'grade_id': 'INT'}),
+	# 		 (3, {'labelV': 'classroom', 'classroom_id': 'INT', 'grade_id': 'INT', 'section': 'VARCHAR', 'teacher_id': 'INT'}),
+	# 		 (4, {'labelV': 'classroom_student', 'classroom_id': 'INT', 'studen_id': 'INT'}), (5, {'labelV': 'attendance', 'date': 'Date', 'student_id': 'INT', 'status': 'INT'}),
+	# 		 (6, {'exam_type': 'INT', 'name': 'VARCHAR', 'labelV': 'exam_type'}), (7, {'exam_id': 'INT', 'exam_type': 'INT', 'name': 'VARCHAR', 'labelV': 'exam'}), 
+	# 		 (8, {'labelV': 'exam_result', 'exam_id': 'INT', 'student_id': 'INT', 'course_id': 'INT'}), 
+	# 		 (9, {'labelV': 'student', 'student_id': 'INT', 'email': 'VARCHAR', 'name': 'VARCHAR', 'dob': 'Date', 'parent_id': 'INT'}), 
+	# 		 (10, {'labelV': 'parent', 'parent_id': 'INT', 'email': 'VARCHAR', 'password': 'VARCHAR', 'mobile': 'VARCHAR'}), 
+	# 		 (11, {'teacher_id': 'INT', 'name': 'VARCHAR', 'email': 'VARCHAR', 'dob': 'Date', 'labelV': 'teacher'}) ]
 	
-	# edges = [('grade','course',{'labelE':'has'}), ('grade','classroom',{'labelE':'has'}), ('classroom','classroom_student',{'labelE':'has'}),
-	# 		('classroom','teacher',{'labelE':'has'}), ('exam_type','exam',{'labelE':'has'}), ('exam','exam_result',{'labelE':'has'}), ('attendance','student',{'labelE':'has'}),
-	# 		('classroom_student','student',{'labelE':'has'}), ('course','exam_result',{'labelE':'has'}), ('student','exam_result',{'labelE':'has'}),
-	# 		('student','parent',{'labelE':'has'})]
+	# edges = [(1, 2, {'labelE': 'has'}), (1, 3, {'labelE': 'has'}), (3, 4, {'labelE': 'has'}), (3, 11, {'labelE': 'has'}),
+	# 		 (6, 7, {'labelE': 'has'}), (7, 8, {'labelE': 'has'}), (5, 9, {'labelE': 'has'}), (4, 9, {'labelE': 'has'}), 
+	# 		 (2, 8, {'labelE': 'has'}), (9, 8, {'labelE': 'has'}), (9, 10, {'labelE': 'has'})]
 
-	# nodes = [('Area',{'labelV':'node','Area_id':'INT','Name':'VARCHAR','Subjects_id':'INT'}),('Subject',{'labelV':'node','Subject_id':'INT','Abbreviation':'VARCHAR','Area_id':'INT','ScoreRecords_id':'INT','SubjectGrades_id':'INT'}),
-	# 		('SubjectGrade',{'labelV':'node','SubjectGrade_id':'INT','Grade_id':'INT','Subject_id':'INT'}),('Level',{'labelV':'node','Level_id':'INT','Name':'VARCHAR','Principle':'VARCHAR','Garde_id':'INT'}), 
-	# 		('Grade',{'labelV':'node','Garde_id':'INT','Name':'VARCHAR','Level_id':'INT','Observation':'VARCHAR','Grade_paraleloes_id':'INT','Subject_grades_id':'INT'}),
-	# 		('ScoreRecord',{'labelV':'node','ScoreRecord_id':'INT','Subject_id':'INT','Student_id':'INT','FirstTrimester':'VARCHAR','SecondTrimester':'VARCHAR','ThirdTrimester':'VARCHAR','FinalGrade':'INT','Year':'DATE'}),
-	# 		('Attendance',{'labelV':'node','Attendance_id':'INT','Student_id':'INT','Attended':'VARCHAR','Date':'DATE'}),('Student',{'labelV':'node','Student_id':'INT','Garde_paralelo_id':'INT','Rude':'VARCHAR','Attendance_id':'INT','ScoreRecords_id':'INT','Name':'VARCHAR','Father_name':'VARCHAR','Mother_name':'VARCHAR','Sex':'VARCHAR','Date_of_Birth':'DATE','Mobile_phone':'VARCHAR','Address':'VARCHAR'}),
-	# 		('GradeParalelo',{'labelV':'node','Garde_paralelo_id':'INT','Grade_id':'INT','Staff_id':'INT','Name':'VARCHAR','Student_id':'INT'}),
-	# 		('Staff',{'labelV':'node','Staff_id':'INT','Name':'VARCHAR','Date_of_birth':'DATE','Place_of_birth':'VARCHAR','Sex':'VARCHAR','Mobile_phone':'INT','Address':'VARCHAR','Father_name':'VARCHAR','Mother_name':'VARCHAR','Salary':'VARCHAR','StaffType_id':'INT','Garde_paralelos_id':'INT'}),
-	# 		('User',{'labelV':'node','Username':'VARCHAR','Password':'VARCHAR'}),('StaffType',{'labelV':'node','Name':'VARCHAR','Staff_id':'INT'})]
+	# nodes = [(12, {'labelV': 'Area', 'Area_id': 'INT', 'Name': 'VARCHAR', 'Subjects_id': 'INT'}), (13, {'labelV': 'Subject', 'Subject_id': 'INT', 'Abbreviation': 'VARCHAR', 'Area_id': 'INT', 'ScoreRecords_id': 'INT', 'SubjectGrades_id': 'INT'}), 
+	# 		(14, {'labelV': 'SubjectGrade', 'SubjectGrade_id': 'INT', 'Grade_id': 'INT', 'Subject_id': 'INT'}), (15, {'labelV': 'Level', 'Level_id': 'INT', 'Name': 'VARCHAR', 'Principle': 'VARCHAR', 'Garde_id': 'INT'}), 
+	# 		(16, {'labelV': 'Grade', 'Garde_id': 'INT', 'Name': 'VARCHAR', 'Level_id': 'INT', 'Observation': 'VARCHAR', 'Grade_paraleloes_id': 'INT', 'Subject_grades_id': 'INT'}), 
+	# 		(17, {'labelV': 'ScoreRecord', 'ScoreRecord_id': 'INT', 'Subject_id': 'INT', 'Student_id': 'INT', 'FirstTrimester': 'VARCHAR', 'SecondTrimester': 'VARCHAR', 'ThirdTrimester': 'VARCHAR', 'FinalGrade': 'INT', 'Year': 'DATE'}), 
+	# 		(18, {'labelV': 'Attendance', 'Attendance_id': 'INT', 'Student_id': 'INT', 'Attended': 'VARCHAR', 'Date': 'DATE'}), 
+	# 		(19, {'labelV': 'Student', 'Student_id': 'INT', 'Garde_paralelo_id': 'INT', 'Rude': 'VARCHAR', 'Attendance_id': 'INT', 'ScoreRecords_id': 'INT', 'Name': 'VARCHAR', 'Father_name': 'VARCHAR', 'Mother_name': 'VARCHAR', 'Sex': 'VARCHAR', 'Date_of_Birth': 'DATE', 'Mobile_phone': 'VARCHAR', 'Address': 'VARCHAR'}), 
+	# 		(20, {'labelV': 'GradeParalelo', 'Garde_paralelo_id': 'INT', 'Grade_id': 'INT', 'Staff_id': 'INT', 'Name': 'VARCHAR', 'Student_id': 'INT'}), 
+	# 		(21, {'labelV': 'Staff', 'Staff_id': 'INT', 'Name': 'VARCHAR', 'Date_of_birth': 'DATE', 'Place_of_birth': 'VARCHAR', 'Sex': 'VARCHAR', 'Mobile_phone': 'INT', 'Address': 'VARCHAR', 'Father_name': 'VARCHAR', 'Mother_name': 'VARCHAR', 'Salary': 'VARCHAR', 'StaffType_id': 'INT', 'Garde_paralelos_id': 'INT'}), 
+	# 		(22, {'labelV': 'User', 'Username': 'VARCHAR', 'Password': 'VARCHAR'}), (23, {'labelV': 'StaffType', 'Name': 'VARCHAR', 'Staff_id': 'INT'})]
 	
-	# edges = [('Subject','Area',{'labelE':'Area_info'}),('SubjectGrade','Subject',{'labelE':'of_Subject'}),('ScoreRecord','Subject',{'labelE':'subject_info'}),
-	# 		('Grade','SubjectGrade',{'labelE':'level_info'}),
-	# 		('Grade','Level',{'labelE':'has'}), ('Grade','GradeParalelo',{'labelE':'has'}), ('GradeParalelo','Student',{'labelE':'has'}),
-	# 		('Student','ScoreRecord',{'labelE':'has'}),('Student','Attendance',{'labelE':'has'}),('GradeParalelo','Staff',{'labelE':'has'}),
-	# 		('Staff','StaffType')]
+	# edges = [(13, 12, {'labelE': 'Area_info'}), (14, 13, {'labelE': 'of_Subject'}), (17, 13, {'labelE': 'subject_info'}), 
+	# 		(16, 14, {'labelE': 'level_info'}), (16, 15, {'labelE': 'has'}), (16, 20, {'labelE': 'has'}), 
+	# 		(20, 19, {'labelE': 'has'}), (19, 17, {'labelE': 'has'}), (19, 18, {'labelE': 'has'}), 
+	# 		(20, 21, {'labelE': 'has'}), (21, 23)]
+
+
+
 	#g3 = gra.create_graph(nodes,edges)
 	# vet = Vertex(G2)
 	# print(vet.list_all())
@@ -198,7 +204,7 @@ if __name__=="__main__":
 	#vet.add_vertex('4',data)
 	#ed.add_edge('grade','course','')
 
-	nx.draw(G,with_labels=True)
-	plt.show()
+	#nx.draw(G,with_labels=True)
+	#plt.show()
 	#nx.write_graphml(G1, "graph1.graphml")
 	#print(vet.properties('142'))
